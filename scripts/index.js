3c70d41c068cf0aaf803ad56c1a9e8fe8395b530
Chris Aquino <chris@radishmouse.com>
2019-03-12 14:55:05 -0400

    draws emails to listing area

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,40 @@
 // Constants
 // ============================================
 const API_URL = 'https://dc-coffeerun.herokuapp.com/api/coffeeOrders';
+const LIST_AREA = '[data-listing]';
 
 // ============================================
 // DOM Element Creation
 // ============================================
+
+function emailElement(email) {
+  const emailAnchor = document.createElement('a');
+  emailAnchor.textContent = email;
+
+  const emailItem = document.createElement('li');
+  emailItem.appendChild(emailAnchor);
+
+  return emailItem;
+}
+
+function drawSingleEmailToList(email) {
+  const listArea = document.querySelector(LIST_AREA);
+  listArea.appendChild(emailElement(email));  
+}
+
+function drawAllEmailsToList(emailArray) {
+  emailArray.forEach(drawSingleEmailToList);  
+}
+
+
+// ============================================
+// Data manipulation functions
+// ============================================
+
+function getEmails(coffeeOrdersObject) {
+  return Object.keys(coffeeOrdersObject);
+}
+
 
 // ============================================
 // API functions
@@ -15,7 +45,10 @@ function getCoffeeOrders() {
   fetch(API_URL)
     .then(function (response) { return response.json(); })
     .then(function (coffeeOrdersObject) {
-      console.log(coffeeOrdersObject);
+      // console.log(getEmails(coffeeOrdersObject));
+      drawAllEmailsToList(getEmails(coffeeOrdersObject));
+      
+      return coffeeOrdersObject;
     });
 }
 
