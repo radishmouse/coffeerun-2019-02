7cb148c530b5c36cfdff8453683700c4662d9fb9
Chris Aquino <chris@radishmouse.com>
2019-03-13 09:04:21 -0400

    attaches listener for showing full order

--- scripts/index.js
+++ scripts/index.js
@@ -14,6 +14,9 @@ function emailElement(email) {
   const emailAnchor = document.createElement('a');
   emailAnchor.textContent = email;
 
+  // Setting this as the href, so I can refer to it later.
+  emailAnchor.setAttribute('href', email);
+
   const emailItem = document.createElement('li');
   emailItem.appendChild(emailAnchor);
 
@@ -22,11 +25,37 @@ function emailElement(email) {
 
 function drawSingleEmailToList(email) {
   const listArea = document.querySelector(LIST_AREA);
-  listArea.appendChild(emailElement(email));  
+  const emailItem = emailElement(email);
+  listArea.appendChild(emailItem);
+  return emailItem;
 }
 
 function drawAllEmailsToList(emailArray) {
-  emailArray.forEach(drawSingleEmailToList);  
+  return emailArray.map(drawSingleEmailToList);
+}
+
+function drawFullOrderToDetail(orderInfo) {
+  console.log(orderInfo);
+}
+
+function attachListItemHandler(item, coffeeOrdersObject) {
+  // We know that each list item looks like this:
+  // <li><a href="me@me.com">me@me.com</a></li>
+  // We can access the email address by doing this:
+  // 1. Get the anchor tag that's inside the <li>
+  const anchor = item.querySelector('a');
+  // Alternatively, we could have used: item.children[0];
+
+  // 2. Get the value of the "href"
+  const email = anchor.getAttribute('href');
+  // And as an alternative we could used: anchor.textContent
+
+  // Now, grab the full order for that email address
+  anchor.addEventListener('click', function (event) {
+    event.preventDefault();
+    const fullOrder = getCoffeeForEmail(email, coffeeOrdersObject);
+    drawFullOrderToDetail(fullOrder);    
+  });
 }
 
 
@@ -36,6 +65,10 @@ function drawAllEmailsToList(emailArray) {
 
 function getEmails(coffeeOrdersObject) {
   return Object.keys(coffeeOrdersObject);
+}
+
+function getCoffeeForEmail(email, coffeeOrdersObject) {
+  return coffeeOrdersObject[email];
 }
 
 
@@ -48,8 +81,12 @@ function getCoffeeOrders() {
     .then(function (response) { return response.json(); })
     .then(function (coffeeOrdersObject) {
       // console.log(getEmails(coffeeOrdersObject));
-      drawAllEmailsToList(getEmails(coffeeOrdersObject));
-      
+      const listItems = drawAllEmailsToList(getEmails(coffeeOrdersObject));
+      console.log(listItems.length);
+      listItems.forEach(function (item) {
+        attachListItemHandler(item, coffeeOrdersObject);
+      });
+
       return coffeeOrdersObject;
     });
 }
